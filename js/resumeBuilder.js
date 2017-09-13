57c0e22c339402be97cc34d9addae9d9f2903eea
kfuerniss <kfuernis@swbell.net>
2017-09-12 19:51:55 -0700

    Initial commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,26 @@
+/*
+
+	var bio = {
+		"name" : "Kim Fuerniss",
+		"role" : "Applications Developer",
+		"contacts": {
+			"mobile": "210-882-8288",
+			"email": "kfuernis@swbell.net",
+			"github": "kfuerniss",
+			"location": "Henderson, NV"
+			},
+		"skills": [
+			"Verint", "JavaScript", "HTML"
+			],
+		"bioPic":"image/kim.jpg"
+	}
+	var formattedName = HTMLheaderName.replace(%data%, name);
+	
+	var role = "Web Developer";
+	var formattedRole = HTMLheaderRole.replace(%data%, role);
+
+	$("#header").prepend(formattedRole);
+	$("#header").append(formattedName);
+
+
+ */
